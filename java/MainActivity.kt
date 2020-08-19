5c041c44b7a04c19a6ab8a42145464f04eb9bfce
adventitious <47214398+adventitious@users.noreply.github.com>
2020-08-19 14:07:22 +0100

    Add files via upload

--- java/MainActivity.kt
+++ java/MainActivity.kt
@@ -6,42 +6,97 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.View.*
 
 const val EXTRA_MESSAGE = "com.example.myappaug2020.MESSAGE"
 
 
 class MainActivity : AppCompatActivity() {
+
+//    var backOutOk: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.decorView.setOnSystemUiVisibilityChangeListener { visibility ->
+            // Note that system bars will only be "visible" if none of the
+            // LOW_PROFILE, HIDE_NAVIGATION, or FULLSCREEN flags are set.
+            if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
+                // TODO: The system bars are visible. Make any desired
+                // adjustments to your UI, such as showing the action bar or
+                // other navigational controls.
+/*
+                if( backOutOk == false)
+                {
+                    playMusic( 3 )
+                }
+*/
+
+                Log.i("qqq", "bars visible")
+
+
+            } else {
+                // TODO: The system bars are NOT visible. Make any desired
+                // adjustments to your UI, such as hiding the action bar or
+                // other navigational controls.
+
+                Log.i("qqq", "bars Not visible")
+            }
+        }
+
+
+
+        window.decorView.apply {
+            systemUiVisibility =  View.SYSTEM_UI_FLAG_IMMERSIVE  or View.SYSTEM_UI_FLAG_FULLSCREEN  or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
 
 
         Log.i("qqq", "created")
     }
 
 
-    fun Song1(view: View){
-        playMusic( "Song One" )
+    fun song1(view: View){
+        playMusic( 1 )
+    }
+
+    fun song2(view: View){
+
+        //backOutOk = true
+        //finish()
+        playMusic( 2 )
+    }
+
+    fun song3(view: View){
+        playMusic( 3 )
+    }
+
+    fun song4(view: View){
+        playMusic( 4 )
+    }
+
+    fun song5(view: View){
+        playMusic( 5 )
+    }
+
+    fun song6(view: View){
+        playMusic( 6 )
+    }
+
+    fun song7(view: View){
+        playMusic( 7 )
+    }
+
+    fun song8(view: View){
+        playMusic( 8 )
     }
 
 
-    fun Song2(view: View){
-        playMusic( "Song Two" )
-    }
 
-
-    fun playMusic( songName: String ) {
+    fun playMusic( songId: Int ) {
 
         val intent = Intent(this, musicPlayer::class.java).apply {
-            putExtra(EXTRA_MESSAGE, songName)
-        }
-        startActivity(intent)
-    }
-
-    fun playMusic2(view: View){
-
-        val intent = Intent(this, musicPlayer::class.java).apply {
-            putExtra(EXTRA_MESSAGE, "hello")
+            putExtra(EXTRA_MESSAGE, "" + songId + "")
         }
         startActivity(intent)
     }
--- java/musicPlayer.kt
+++ java/musicPlayer.kt
@@ -1,39 +1,112 @@
 package com.example.appaug2020
 
+import android.content.Intent
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class musicPlayer : AppCompatActivity() {
-
 
     var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
         setContentView(R.layout.activity_music_player)
 
-        val songName = intent.getStringExtra(EXTRA_MESSAGE)
-
-        val textView = findViewById<TextView>(R.id.textView2).apply {
-            text = songName
+        window.decorView.apply {
+            systemUiVisibility =  View.SYSTEM_UI_FLAG_IMMERSIVE  or View.SYSTEM_UI_FLAG_FULLSCREEN  or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         }
 
-        if(songName == "Song One")
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        var songId: Int = 1
+
+        try {
+            songId = Integer.parseInt(message)
+        }
+        catch ( eq: NumberFormatException )
         {
-            mediaPlayer = MediaPlayer.create(this, R.raw.sound1)
+            Log.i("qqq", " exc ")
+        }
+
+        val imageView = findViewById<ImageView>(R.id.imageView7)
+
+        if(songId == 1)
+        {
+            imageView.setImageResource(R.drawable.batman);
+
+            mediaPlayer = MediaPlayer.create(this, R.raw.batman)
             mediaPlayer?.start()
         }
 
-        if(songName == "Song Two")
+        if(songId == 2)
         {
-            mediaPlayer = MediaPlayer.create(this, R.raw.sound2)
+            imageView.setImageResource(R.drawable.grease);
+
+            mediaPlayer = MediaPlayer.create(this, R.raw.grease)
             mediaPlayer?.start()
         }
 
 
+        if(songId == 3)
+        {
+            imageView.setImageResource(R.drawable.underwater_waltz);
 
+            mediaPlayer = MediaPlayer.create(this, R.raw.underwater_waltz)
+            mediaPlayer?.start()
+        }
+
+        if(songId == 4)
+        {
+            imageView.setImageResource(R.drawable.the_sting);
+
+            mediaPlayer = MediaPlayer.create(this, R.raw.the_sting)
+            mediaPlayer?.start()
+        }
+
+        if(songId == 5)
+        {
+            imageView.setImageResource(R.drawable.axel_f);
+
+            mediaPlayer = MediaPlayer.create(this, R.raw.axel_f)
+            mediaPlayer?.start()
+        }
+
+        if(songId == 6)
+        {
+            imageView.setImageResource(R.drawable.phantom);
+
+            mediaPlayer = MediaPlayer.create(this, R.raw.phantom)
+            mediaPlayer?.start()
+        }
+
+        if(songId == 7)
+        {
+            imageView.setImageResource(R.drawable.tetris_a);
+
+            mediaPlayer = MediaPlayer.create(this, R.raw.tetris_a)
+            mediaPlayer?.start()
+        }
+
+        if(songId == 8)
+        {
+            imageView.setImageResource(R.drawable.tetris_b);
+
+            mediaPlayer = MediaPlayer.create(this, R.raw.tetris_b)
+            mediaPlayer?.start()
+        }
+    }
+
+    fun backOut(view: View){
+        val intent = Intent(this , MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
 
